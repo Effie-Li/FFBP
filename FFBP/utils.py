3cffb56fe33d7c69946ddde169ae38ef6fa0946f
alexten <tenalexander1991@gmail.com>
2017-10-21 22:40:44 +0600

    big commit

--- FFBP/utils.py
+++ FFBP/utils.py
@@ -21,40 +21,111 @@ def retrieve_model_params(path_to_event_file, layer_name, param_name):
     return params_dict
 
 
+def get_layer_snapshot(path_to_event_file, layer_name, epoch, pattern_label, target=False):
+    '''
+    
+    :param path_to_event_file: 
+    :param epoch: 
+    :param pattern_label: 
+    :param target: 
+    :return: snapshot = dict(input_pattern=[], weights=[], biases=[], net_input=[], activation=[],
+                    gweights=[], gbiases=[], gnet_input=[], gactivation=[], loss=[])
+    '''
+    snapshot = dict(input_pattern=[], weights=[], biases=[], net_input=[], activation=[],
+                    gweights=[], gbiases=[], gnet_input=[], gactivation=[], loss=[])
+    if target: fields.append('target')
+    
+    events = []
+    for event in tf.train.summary_iterator(path_to_event_file):
+        if event.step == epoch:
+            events.append(event)
+    
+    labels = []
+    for event in events:
+        for val in event.summary.value:
+            if 'pattern_labels' in val.tag: labels.append(val.tensor.string_val[0].decode('utf-8'))
+            if 'input_patterns' in val.tag: snapshot['input_pattern'].append(tf.contrib.util.make_ndarray(val.tensor))
+            if 'target_patterns' in val.tag and target: snapshot['target_pattern'].append(tf.contrib.util.make_ndarray(val.tensor))
+            if 'test_loss_summary' in val.tag: snapshot['loss'].append(val.simple_value)
+                
+            if '/'.join([layer_name,'weights']) in val.tag: snapshot['weights'].append(tf.contrib.util.make_ndarray(val.tensor))
+            if '/'.join([layer_name, 'biases']) in val.tag: snapshot['biases'].append(tf.contrib.util.make_ndarray(val.tensor))
+    
+            if '/'.join([layer_name, 'net_input']) in val.tag: snapshot['net_input'].append(tf.contrib.util.make_ndarray(val.tensor))
+            if '/'.join([layer_name, 'activation']) in val.tag: snapshot['activation'].append(tf.contrib.util.make_ndarray(val.tensor))
+            if '/'.join([layer_name, 'gradient_weights']) in val.tag: snapshot['gweights'].append(tf.contrib.util.make_ndarray(val.tensor))
+            if '/'.join([layer_name, 'gradient_biases']) in val.tag: snapshot['gbiases'].append(tf.contrib.util.make_ndarray(val.tensor))
+            if '/'.join([layer_name, 'gradient_net_input']) in val.tag: snapshot['gnet_input'].append(tf.contrib.util.make_ndarray(val.tensor))
+            if '/'.join([layer_name, 'gradient_activation']) in val.tag: snapshot['gactivation'].append(tf.contrib.util.make_ndarray(val.tensor))
+    
+    ind = labels.index(pattern_label)
+    for key, snap_val in snapshot.items():
+        snapshot[key] = snap_val[ind]
+        
+    return snapshot
+
+
+def get_epochs_and_labels(path_to_event_file):
+    epochs = []
+    labels = []
+    got_labels = False
+    for event in tf.train.summary_iterator(path_to_event_file):
+        for val in event.summary.value:
+            if 'test_loss_summary' in val.tag: epochs.append(event.step)
+        if not got_labels:
+            for val in event.summary.value:
+                if 'pattern_labels' in val.tag:
+                    labels.append(val.tensor.string_val[0].decode('utf-8'))
+            got_labels = True
+    return list(set(epochs)), labels
+
+
 def retrieve_model_data(path_to_event_file, layer_name, tensor_name):
     '''
     Retrieves model data from a tensorflow event file and structures it into lists within a named tuple withina an 
-    ordered dict: 
+    ordered dict
     :param path_to_event_file: absolute path to event file
     :param layer_name: string name of the layer
     :param tensor_name: string name of the data to retrieve (e.g. 'net_input')
-    :return data_dict: OrderedDict = {epoch_num: namedtuple(labels: [], inputs: [], targets: [], data: [])}
+    :return data_dict: 
+    example: OrderedDict = {epoch_num: namedtuple(labels: [t1,t2], inputs: [t1,t2], targets: [t1,t2], data: [t1,t2])}
     '''
-    SummaryData = namedtuple('SummaryData', ['labels', 'inputs', 'targets', 'data'])
     data_dict = OrderedDict()
+    SummaryData = namedtuple('SummaryData', ['labels', 'inputs', 'targets', 'data'])
     lookup = '/'.join([layer_name, tensor_name])
-    for i, event in enumerate(tf.train.summary_iterator(event_file)):
-        data_dict.setdefault(event.step, SummaryData([],[],[],[]))
+    for event in tf.train.summary_iterator(path_to_event_file):
+        data_dict.setdefault(event.step, SummaryData([], [], [], []))
         for val in event.summary.value:
-            if 'input_patterns' in val.tag:
+            if 'pattern_labels' in val.tag:
+                data_dict[event.step].labels.append(val.tensor.string_val[0].decode('utf-8'))
+            elif 'input_patterns' in val.tag:
                 data_dict[event.step].inputs.append(tf.contrib.util.make_ndarray(val.tensor))
-            if 'target_patterns' in val.tag:
+            elif 'target_patterns' in val.tag:
                 data_dict[event.step].targets.append(tf.contrib.util.make_ndarray(val.tensor))
-            if lookup in val.tag:
+            elif lookup in val.tag:
                 data_dict[event.step].data.append(tf.contrib.util.make_ndarray(val.tensor))
-
+        if not sum([len(field) for field in data_dict[event.step]]): data_dict.pop(event.step)
     return data_dict
 
 
 def retrieve_loss(path_to_event_file):
-    eacc = get_event_accumulator(path_to_event_file)
-    loss_log = np.stack([np.asarray([scalar.step, scalar.value]) for scalar in eacc.Scalars('train/error_summary')])
-    return loss_log
+    lookup = 'train_loss_summary'
+    epochs, loss_vals = [], []
+    
+    for event in tf.train.summary_iterator(path_to_event_file):
+        for val in event.summary.value:
+            if lookup in val.tag:
+                epochs.append(event.step)
+                loss_vals.append(val.simple_value)
+                
+    return np.column_stack([epochs, loss_vals])
 
 
 def display_model_data(data_dict):
     for step, data in data_dict.items():
         print('> Epoch',step)
+        print('  labels:')
+        for x in data.labels: print(x)
         print('  inputs:')
         for x in data.inputs: print(x)
         print('  targets:')
--- FFBP/vis_utils.py
+++ FFBP/vis_utils.py
@@ -8,7 +8,7 @@ from mpl_toolkits.axes_grid import Divider
 import matplotlib.pyplot as plt
 import numpy as np
 
-from FFBP.utils import retrieve_loss, retrieve_array
+import FFBP.utils as utils
 from PDPATH import PDPATH
 
 
@@ -33,6 +33,12 @@ def _hide_ticks(ax):
         labelright='off')
 
 
+def _add_grid(figure, rect, divider, x, coords, colormap, vrange):
+    ax = figure.add_axes(rect, xticks=[], yticks=[])
+    ax.set_axes_locator(divider.new_locator(nx=coords[0], ny=coords[1]))
+    ax.imshow(x, cmap=colormap, vmin=vrange[0], vmax=vrange[1])
+
+
 class Logdir(object):
     def __init__(self, path_to_logdir=PDPATH('/xor/train')):
         ls = os.listdir(path_to_logdir)
@@ -43,78 +49,86 @@ class Logdir(object):
                                                                  disabled = False)
                                  )
         display(self.widget)
-        
-    def log_path(self):
-        return '/'.join([self.__logdir_path__, self.widget.result])
-        
+        self.log_path = '/'.join([self.__logdir_path__, self.widget.result])
+        self.event_path = os.path.join(self.log_path, os.listdir(self.log_path)[0])
+
 
 def view_error(logdir):
-    data = retrieve_loss(logdir.log_path())
+    data = utils.retrieve_loss(logdir.event_path)
     plt.plot(data[:,0], data[:,1])
+    plt.grid()
 
     
-def _draw_layer_(fig, data, colormap, step_ind, pat_ind, vrange):
+def _draw_layer_(fig, logdir, layer_name, colormap, epoch, pattern_label, vrange):
+    # TODO !!!!!!!
+    # Perhaps, draw everything upon the view_layer() call and make draw layer just insert and draw the arrays into their respective grids
+    
+    NUM_GRIDS = 5
+    data = utils.get_layer_snapshot(logdir.event_path, layer_name, epoch, pattern_label, target=False)
     rect = (0.1, 0.1, 0.8, 0.8) 
-    axs = [fig.add_axes(rect, label='{}'.format(i)) for i in range(len(data.keys()))]
     
-    max_width, max_height = np.shape(data['weights'][step_ind])
+    # axs = [fig.add_axes(rect, label='{}'.format(i)) for i in range(NUM_GRIDS)]
+    
+    max_width, max_height = 2,2
     col_pad, row_pad = Size.Scaled(.8), Size.Scaled(.8)
-    
+
     col_widths = [Size.Scaled(max_width), col_pad,
                   Size.Scaled(1), col_pad,
                   Size.Scaled(1), col_pad,
                   Size.Scaled(1)]
 
-    row_heights = [Size.Scaled(1), row_pad,
-                   Size.Scaled(max_height)]
 
-    # divide the axes rectangle into grid whose size is specified by horiz * vert
+    row_heights = [Size.Scaled(1), row_pad,
+               Size.Scaled(max_height)]
     divider = Divider(fig, rect, col_widths, row_heights, aspect=True)
     
-    params = {
-        'input':{'locs':(0,0), 'two_inds':True},
-        'weights':{'locs':(0,2),'two_inds':False},
-        'biases':{'locs':(2,2),'two_inds':False},
-        'net_input':{'locs':(4,2), 'two_inds':True},
-        'activations':{'locs':(6,2), 'two_inds':True}
-    }
+    # params = {
+    #     'input':{'locs':(0,0), 'two_inds':True},
+    #     'weights':{'locs':(0,2),'two_inds':False},
+    #     'biases':{'locs':(2,2),'two_inds':False},
+    #     'net_input':{'locs':(4,2), 'two_inds':True},
+    #     'activations':{'locs':(6,2), 'two_inds':True}
+    # }
+    # 
+    # axs = {}
+    # for i, (key, val) in enumerate(data.items()):
+    #     axs[key] = fig.add_axes(rect, label=key, xticks=[], yticks=[])
+    #     axs[key].set_axes_locator(divider.new_locator(nx=params[key]['locs'][0], ny=params[key]['locs'][1]))
+    #     x = val[step_ind][pat_ind] if params[key]['two_inds'] else val[step_ind]
+    #     axs[key].imshow(x, cmap=colormap, vmin=vrange[0], vmax=vrange[1])
     
-    axs = {}
-    for i, (key, val) in enumerate(data.items()):
-        axs[key] = fig.add_axes(rect, label=key, xticks=[], yticks=[])
-        axs[key].set_axes_locator(divider.new_locator(nx=params[key]['locs'][0], ny=params[key]['locs'][1]))
-        x = val[step_ind][pat_ind] if params[key]['two_inds'] else val[step_ind]
-        axs[key].imshow(x, cmap=colormap, vmin=vrange[0], vmax=vrange[1])
+    _add_grid(fig, rect, divider, data['input_pattern'], (0,0), colormap, vrange)
+    _add_grid(fig, rect, divider, data['weights'], (0,2), colormap, vrange)
+    _add_grid(fig, rect, divider, data['biases'], (2,2), colormap, vrange)
+    _add_grid(fig, rect, divider, data['net_input'], (4,2), colormap, vrange)
+    _add_grid(fig, rect, divider, data['activation'], (6,2), colormap, vrange)
+
+    
     plt.draw()
     plt.show()
 
 
-def view_layer(logdir, layer_name, _target=False):
-    path = logdir.log_path()
-    data = {'weights': None, 'net_input': None, 'biases': None, 'activations': None}
-    for key in data.keys():
-        data[key], steps = retrieve_array(path, layer_name, key)
-        # print('retrieving {}:\n{}'.format(key, data[key]))
-    print('Fake input of length:',data['weights'][0].shape[1])
-    data['input'] = [np.zeros([4, data['weights'][0].shape[0]]) for i in steps]
+def view_layer(logdir, layer_name, target=False):
     fig = plt.figure()
+    path = logdir.event_path
+    epochs, labels = utils.get_epochs_and_labels(path)
 
-    # Axes grid is a rectangle placed inside a figure. specify the position of the rectange in the figure: 
-    # (lower-left corner coords, upper-right corner coords), or (x1,y1, x2,y2)
-    rect = (0.1, 0.1, 0.8, 0.8)
-    max_height, max_width = np.shape(data['weights'][0])
-    col_pad = Size.Scaled(.8)
-    row_pad = Size.Scaled(.8)
-    col_widths = [Size.Scaled(max_width), col_pad,
-                  Size.Scaled(1), col_pad,
-                  Size.Scaled(1), col_pad,
-                  Size.Scaled(1)]
-
-    row_heights = [Size.Scaled(1), row_pad,
-                   Size.Scaled(max_height)]
-
-    # divide the axes rectangle into grid whose size is specified by horiz * vert
-    Divider(fig, rect, col_widths, row_heights, aspect=True)
+    # # Axes grid is a rectangle placed inside a figure. specify the position of the rectange in the figure: 
+    # # (lower-left corner coords, upper-right corner coords), or (x1,y1, x2,y2)
+    # rect = (0.1, 0.1, 0.8, 0.8)
+    # max_height, max_width = 2,2
+    # col_pad = Size.Scaled(.8)
+    # row_pad = Size.Scaled(.8)
+    # col_widths = [Size.Scaled(max_width), col_pad,
+    #               Size.Scaled(1), col_pad,
+    #               Size.Scaled(1), col_pad,
+    #               Size.Scaled(1)]
+    # 
+    # row_heights = [Size.Scaled(1), row_pad,
+    #                Size.Scaled(max_height)]
+    # 
+    # # divide the axes rectangle into grid whose size is specified by horiz * vert
+    # Divider(fig, rect, col_widths, row_heights, aspect=True)
     
     cmap_widget = widgets.Dropdown(
         options = sorted(['BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr', 'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'seismic']),  
@@ -130,9 +144,9 @@ def view_layer(logdir, layer_name, _target=False):
         continuous_update=False)
     
     step_widget = widgets.IntSlider(
-        value = steps[0],
+        value = epochs[0],
         min = 0,
-        max = len(steps)-1,
+        max = len(epochs)-1,
         step = 1,
         description='Step index: ',
         continuous_update=False)
@@ -140,8 +154,9 @@ def view_layer(logdir, layer_name, _target=False):
     interact(
         _draw_layer_,
         fig = fixed(fig),
-        data = fixed(data),
-        step_ind = step_widget,
-        pat_ind = fixed(0),
+        logdir = fixed(logdir),
+        layer_name = fixed(layer_name),
+        epoch = step_widget,
+        pattern_label = fixed('p00'),
         colormap = cmap_widget,
         vrange=vrange_widget)
