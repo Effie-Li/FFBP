f520588ccc0d7d8fdf85272a257f065f07efea14
alexten <tenalexander1991@gmail.com>
2017-11-16 22:38:42 +0600

    Add old vis_utils functions

--- FFBP/dump.py
+++ FFBP/dump.py
@@ -75,6 +75,206 @@ def view_error(logdir):
     plt.grid()
 
 
+# import os
+# from os.path import join as joinp
+# from ipywidgets import interact, fixed
+# import ipywidgets as widgets
+#
+# import pickle
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from matplotlib.cm import get_cmap
+# from mpl_toolkits.axes_grid1 import SubplotDivider, LocatableAxes
+# from mpl_toolkits.axes_grid1.axes_size import Scaled
+#
+# from .utils import get_layer_dims, get_epochs, get_pattern_options
+
+
+def _make_axes_grid(mpl_figure, N, subplot_ind, layer_size, layer_inp_size, target=False):
+    '''
+    DOCUMENTATION
+    :param mpl_figure: an instance of matplotlib.figure.Figure
+    :param N: number of layers
+    :param i: layer index
+    :param layer_size: number units in the layer
+    :param layer_inp_size: number of sending units to the layer
+    :param target: include target
+    :return:
+    '''
+    # define padding size
+    _ = Scaled(.8)
+
+    # define grid column sizes (left to right): weights, biases, net_input, output, gweight, gbiases, gnet_input, goutput
+    mat_w, cvec_w = Scaled(layer_size), Scaled(1)
+    left_panel = [mat_w, _, cvec_w, _, cvec_w, _, cvec_w, _]
+    right_panel = [_, mat_w, _, cvec_w, _, cvec_w, _, cvec_w]
+    cols =  left_panel + [cvec_w,_] + right_panel if target else left_panel + right_panel
+
+    # define grid row sizes (top to bottom): weights, input
+    mat_h, rvec_h = Scaled(layer_inp_size), Scaled(1)
+    rows = [rvec_h, _, mat_h]
+
+    # make divider
+    divider = SubplotDivider(mpl_figure, N, 1, subplot_ind, aspect=True)
+    divider.set_horizontal(cols)
+    divider.set_vertical(rows)
+    # divider = Divider(fig=mpl_figure, pos=frame, horizontal=cols, vertical=rows, aspect=True)
+
+    # provide axes-grid coordinates, image sizes, and titles
+    mat_h, mat_w = layer_inp_size, layer_size
+    t = int(target)
+    ax_params = {
+        'input_':     ((0, 0),      (1, mat_w),     'input'),
+        'weights':    ((0, 2),      (mat_h, mat_w), 'W'    ),
+        'biases':     ((2, 2),      (mat_h, 1),     'b'    ),
+        'net_input':  ((4, 2),      (mat_h, 1),     'net'  ),
+        'output':     ((6, 2),      (mat_h, 1),     'a'    ),
+        'gweights':   ((9+2*t, 2),  (mat_h, mat_w), 'W\''  ),
+        'gbiases':    ((11+2*t, 2), (mat_h, 1),     'b\''  ),
+        'gnet_input': ((13+2*t, 2), (mat_h, 1),     'net\''),
+        'goutput':    ((15+2*t, 2), (mat_h, 1),     'a\''  )
+    }
+    if t:
+        ax_params['targets'] = ((8, 2), (mat_h, 1), 't')
+
+    # create axes and locate appropriately
+    img_dict = {}
+    for k, (ax_coords, img_dims, ax_title) in ax_params.items():
+        ax = LocatableAxes(mpl_figure, divider.get_position())
+        ax.set_axes_locator(divider.new_locator(nx=ax_coords[0], ny=ax_coords[1]))
+        ax.tick_params(labelbottom=False, labelleft=False)
+        ax.set_xticks([]); ax.set_yticks([])
+        if k == 'input_':
+            ax.set_xlabel(ax_title)
+        else:
+            ax.set_title(ax_title)
+        mpl_figure.add_axes(ax)
+        img_dict[k] = ax.imshow(np.zeros(img_dims))
+    return img_dict
+
+
+def _draw_many_layers(snap_path, img_dicts, layer_names, colormap, vrange, tind, pind):
+
+    # pull up required data
+    snap_ldicts = {}
+    with open(snap_path, 'rb') as snap_file:
+        snap = pickle.load(snap_file)
+
+    enum = snap[tind]['enum']
+    loss = snap[tind]['loss'][pind]
+    targ = snap[tind]['target']
+
+    for layer_name in layer_names:
+        snap_ldicts[layer_name] = snap[tind][layer_name]
+        snap_ldicts[layer_name]['targets'] = targ
+
+    del snap # clean up
+
+    print('epoch {}, loss = {:.5f}'.format(enum, loss))
+
+    for img_dict, layer_name in zip(img_dicts, layer_names):
+        for k, img in img_dict.items():
+            data = snap_ldicts[layer_name][k]
+            if k == 'weights':
+                data = data.T
+            if 'biases' in k:
+                data = np.expand_dims(data, axis=1)
+            if data.ndim > 2:
+                data = data[pind]
+                if k != 'input_':
+                    data = data.T
+            img.set_data(data)
+            img.cmap = get_cmap(colormap)
+            img.norm.vmin = vrange[0]
+            img.norm.vmax = vrange[1]
+
+    plt.show()
+
+
+def view_layers(log_path, layer_names, target_on_last=True):
+    filenames = [filename for filename in os.listdir(log_path) if 'log' in filename]
+    runlogs = {}
+    for filename in filenames:
+        runlogs[filename] = joinp(log_path,filename)
+    run_widget = widgets.Dropdown(
+        options = runlogs,
+        description = 'Run log: ',
+        value = runlogs[filenames[0]]
+    )
+
+
+    path = run_widget.value
+    epochs = get_epochs(snap_path=path)
+    layer_dims = get_layer_dims(path, layer_names)
+
+    figure = plt.figure()
+    num_layers = len(layer_names)
+    axes_dicts = []
+
+    disp_targs = [False for l in layer_names]
+    disp_targs[-1] = target_on_last
+
+    for i, (layer_name, disp_targ) in enumerate(zip(layer_names, disp_targs)):
+        axes_dicts.append(
+            _make_axes_grid(
+                mpl_figure=figure,
+                N = num_layers,
+                subplot_ind = i,
+                layer_size=layer_dims[layer_name][0],
+                layer_inp_size=layer_dims[layer_name][1],
+                target=bool(disp_targ))
+        )
+
+    cmap_widget = widgets.Dropdown(
+        options=sorted(['BrBG', 'bwr', 'coolwarm', 'PiYG',
+                        'PRGn', 'PuOr', 'RdBu', 'RdGy',
+                        'RdYlBu', 'RdYlGn', 'seismic']),
+        description='Colors: ',
+        value='coolwarm', disabled=False
+    )
+
+    vrange_widget = widgets.IntRangeSlider(
+        value=[-1, 1],
+        min=-5,
+        max=5,
+        step=1,
+        description='V-range: ',
+        continuous_update=False
+    )
+
+    step_widget = widgets.IntSlider(
+        value=epochs[0],
+        min=0,
+        max=len(epochs) - 1,
+        step=1,
+        description='Step index: ',
+        continuous_update=False
+    )
+
+    pattern_options = get_pattern_options(snap_path=path, tind=step_widget.value)
+    options_map = {}
+    for i, pattern_option in enumerate(pattern_options):
+        options_map[pattern_option] = i
+    pattern_widget = widgets.Select(
+        options=options_map,
+        value=0,
+        rows=min(10, len(pattern_options)),
+        description='Pattern: ',
+        disabled=False
+    )
+
+    interact(
+        _draw_many_layers,
+        snap_path = run_widget,
+        img_dicts = fixed(axes_dicts),
+        layer_names = fixed(layer_names),
+        colormap = cmap_widget,
+        vrange = vrange_widget,
+        tind = step_widget,
+        pind = pattern_widget,
+    )
+
+
 # FFBP.utils
 # ========================
 def retrieve_model_params(path_to_event_file, layer_name, param_name):
@@ -231,3 +431,4 @@ def use_exercise_params(use):
         tf.train.Saver(restore_dict, name='xor_exercise_saver').restore(
             tf.get_default_session(), 'temp/exercise_params_old/exercise_params'
         )
+
