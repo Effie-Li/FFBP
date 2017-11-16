0c63fc6cd2f1850201650dc7a282a46d8ce8af99
alexten <tenalexander1991@gmail.com>
2017-11-16 22:39:07 +0600

    Plot layers in one figure instead of creating different figures

--- FFBP/vis_utils.py
+++ FFBP/vis_utils.py
@@ -1,49 +1,37 @@
 import os
-from IPython.display import display
-from ipywidgets import interact, interactive, fixed, interact_manual
+from os.path import join as joinp
+from ipywidgets import interact, fixed
 import ipywidgets as widgets
 
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
-from mpl_toolkits.axes_grid1 import Divider
+from mpl_toolkits.axes_grid1 import SubplotDivider, LocatableAxes
 from mpl_toolkits.axes_grid1.axes_size import Scaled
 
-import FFBP.utils as utils
-from PDPATH import PDPATH
+from .utils import get_layer_dims, get_epochs, get_pattern_options
 
 
-class Logdir(object):
-    def __init__(self, model_dir):
-        # Create a list containing logdirs in the model directory
-        self._logdirs = os.path.join(PDPATH(), model_dir, 'logdirs')
-        ls = os.listdir(self._logdirs)
-
-        # Create an interactive Dropdown widget for selecting a particular logdir and display it
-        _return_ = lambda x: x
-        self.widget = interactive(_return_, x = widgets.Dropdown(options=ls, description='Logs: ', value=ls[-1], disabled=False))
-        display(self.widget)
-
-        # Set the relevant paths depending on the selected logdir
-        self.path = os.path.join(
-            self._logdirs,
-            self.widget.result
-        )
-        # use [x for x in os.listdir(os.path.join(self._logdirs, self.widget.result)) if '.pkl' in x].pop() in order to
-        # programmatically set path based on file extension
-        self.snap_path = os.path.join(
-            self._logdirs,
-            self.widget.result,
-            'snap.pkl'
-        )
-        # self.event_path = os.path.join(self.log_path, os.listdir(self.log_path)[0]) TODO: identify event file by string name
+def _make_ghost_axis(mpl_figure, rect, title):
+    ghost_ax = mpl_figure.add_axes(rect)
+    [ghost_ax.spines[side].set_visible(False) for side in ['right','top','bottom','left']]
+    ghost_ax.set_xticks([])
+    ghost_ax.set_yticks([])
+    ghost_ax.set_title(title)
 
 
-def _make_axes_grid(mpl_figure, layer_size, layer_inp_size, target=False):
-
-    # make frame to place axes inside figure
-    frame = (0.1, 0.1, 0.8, 0.8) # (x1,y1, x2,y2)
+def _make_axes_grid(mpl_figure, N, subplot_ind, layer_name, layer_size, layer_inp_size, target=False):
+    '''
+    DOCUMENTATION
+    :param mpl_figure: an instance of matplotlib.figure.Figure
+    :param N: number of layers
+    :param i: layer index
+    :param layer_size: number units in the layer
+    :param layer_inp_size: number of sending units to the layer
+    :param target: include target
+    :return:
+    '''
 
     # define padding size
     _ = Scaled(.8)
@@ -59,7 +47,12 @@ def _make_axes_grid(mpl_figure, layer_size, layer_inp_size, target=False):
     rows = [rvec_h, _, mat_h]
 
     # make divider
-    divider = Divider(fig=mpl_figure, pos=frame, horizontal=cols, vertical=rows, aspect=True)
+    divider = SubplotDivider(mpl_figure, N, 1, subplot_ind, aspect=True)
+    divider.set_horizontal(cols)
+    divider.set_vertical(rows)
+
+    # set suptitle
+    _make_ghost_axis(mpl_figure=mpl_figure, rect=divider.get_position(), title=layer_name)
 
     # provide axes-grid coordinates, image sizes, and titles
     mat_h, mat_w = layer_inp_size, layer_size
@@ -81,18 +74,20 @@ def _make_axes_grid(mpl_figure, layer_size, layer_inp_size, target=False):
     # create axes and locate appropriately
     img_dict = {}
     for k, (ax_coords, img_dims, ax_title) in ax_params.items():
-        ax = mpl_figure.add_axes(frame, label=k, xticks=[], yticks=[])
+        ax = LocatableAxes(mpl_figure, divider.get_position())
         ax.set_axes_locator(divider.new_locator(nx=ax_coords[0], ny=ax_coords[1]))
-
+        ax.tick_params(labelbottom=False, labelleft=False)
+        ax.set_xticks([]); ax.set_yticks([])
         if k == 'input_':
             ax.set_xlabel(ax_title)
         else:
             ax.set_title(ax_title)
+        mpl_figure.add_axes(ax)
         img_dict[k] = ax.imshow(np.zeros(img_dims))
     return img_dict
 
 
-def _draw_many_layers(snap_path, figures, img_dicts, layer_names, colormap, vrange, tind, pind):
+def _draw_layers(snap_path, img_dicts, layer_names, colormap, vrange, tind, pind):
 
     # pull up required data
     snap_ldicts = {}
@@ -122,32 +117,47 @@ def _draw_many_layers(snap_path, figures, img_dicts, layer_names, colormap, vran
                 data = data[pind]
                 if k != 'input_':
                     data = data.T
-            img.set_data(data) #, cmap=colormap, vmin=vrange[0], vmax=vrange[1])
+            img.set_data(data)
             img.cmap = get_cmap(colormap)
             img.norm.vmin = vrange[0]
             img.norm.vmax = vrange[1]
-        figures[layer_name].canvas.draw()
 
     plt.show()
 
 
-def view_layers(logdir, layer_names, target_on_last=True):
+def view_layers(log_path, layer_names, target_on_last=True):
+    filenames = [filename for filename in os.listdir(log_path) if 'log' in filename]
+    runlogs = {}
+    for filename in filenames:
+        runlogs[filename] = joinp(log_path,filename)
+    run_widget = widgets.Dropdown(
+        options = runlogs,
+        description = 'Run log: ',
+        value = runlogs[filenames[0]]
+    )
 
-    path = logdir.snap_path
-    epochs = utils.get_epochs(snap_path=path)
 
-    figures = {}
+    path = run_widget.value
+    epochs = get_epochs(snap_path=path)
+    layer_dims = get_layer_dims(path, layer_names)
+
+    figure = plt.figure()
+    num_layers = len(layer_names)
     axes_dicts = []
 
     disp_targs = [False for l in layer_names]
     disp_targs[-1] = target_on_last
 
-    for layer_name, disp_targ in zip(layer_names, disp_targs):
-        figure = plt.figure(num=layer_name.replace('_', ' '))
-        size, inp_size = utils.get_layer_dims(snap_path=path, layer_name=layer_name)
-        figures[layer_name] = figure
+    for i, (layer_name, disp_targ) in enumerate(zip(layer_names, disp_targs)):
         axes_dicts.append(
-            _make_axes_grid(mpl_figure=figure, layer_size=size, layer_inp_size=inp_size, target=bool(disp_targ))
+            _make_axes_grid(
+                mpl_figure=figure,
+                N = num_layers,
+                subplot_ind = i,
+                layer_name=layer_name.upper().replace('_',' '),
+                layer_size=layer_dims[layer_name][0],
+                layer_inp_size=layer_dims[layer_name][1],
+                target=bool(disp_targ))
         )
 
     cmap_widget = widgets.Dropdown(
@@ -176,22 +186,21 @@ def view_layers(logdir, layer_names, target_on_last=True):
         continuous_update=False
     )
 
-    pattern_options = utils.get_pattern_options(snap_path=path, tind=step_widget.value)
+    pattern_options = get_pattern_options(snap_path=path, tind=step_widget.value)
     options_map = {}
     for i, pattern_option in enumerate(pattern_options):
         options_map[pattern_option] = i
     pattern_widget = widgets.Select(
         options=options_map,
         value=0,
-        rows=8,
+        rows=min(10, len(pattern_options)),
         description='Pattern: ',
         disabled=False
     )
 
     interact(
-        _draw_many_layers,
-        snap_path = fixed(logdir.snap_path),
-        figures = fixed(figures),
+        _draw_layers,
+        snap_path = run_widget,
         img_dicts = fixed(axes_dicts),
         layer_names = fixed(layer_names),
         colormap = cmap_widget,
